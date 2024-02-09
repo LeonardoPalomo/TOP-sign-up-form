094106c7dc3a2011acdf800fde462df441ee93c3
LeonardoPalomo <leonardo.palomo@usach.cl>
2024-02-09 15:27:55 -0300

    continue adding features

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TOP-sign-up-form
+
+image from https://imgur.com/a/JjUbH
+font from https://betterstudio.com/fonts/dark-souls-font/
--- password-validator.js
+++ password-validator.js
@@ -1,1 +1,22 @@
-console.log("Hola")
+const password = document.getElementById('password');
+const password_confirm = document.getElementById('confirm');
+let pass_span = document.querySelector(".valid-pass");
+const submit_button = document.querySelector("button[type=\"submit\"");
+const first_name = document.querySelector("#first_name");
+
+console.log(submit_button);
+password_confirm.addEventListener('input', (e) => {
+  if(password.value !== password_confirm.value){
+    pass_span.textContent = "Passwords do not match"
+    pass_span.style="color: red"
+  }
+  else {
+    pass_span.textContent = " "
+  }
+});
+
+submit_button.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(first_name.value);
+});
